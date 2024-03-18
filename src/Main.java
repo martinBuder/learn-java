eef8fc095c70f5c61dc15ce9d647023671660c2b
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2024-03-18 09:17:49 +0100

    first function on the same class

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,21 @@ public class Main {
         String name = "Martin";
         int postcode = 83707;
         String city = "Bad Wiessee";
-
+        String greeting = showGreeting();
+        System.out.println(greeting);
+        System.out.print("My name is " + name + ". I live in " + postcode + " " + city + ".");
+        // Der Unterschied zwischen print und println ist, dass println ein 'enter' hinter der Ausführung hinzufügt.
+    }
+    public static String showGreeting() {
         LocalTime currentTime = LocalTime.now();
         int hour = currentTime.getHour();
-
-        String greeting;
-
         if(hour < 12 && hour > 5) {
-            greeting = "Good Morning! ";
+            return "Good Morning! ";
         } else if (hour > 12 && hour < 20) {
-            greeting = "Good Afternoon! ";
+            return "Good Afternoon! ";
         } else {
-            greeting = "Good Evening! ";
+            return "Good Evening! ";
         }
 
-
-        System.out.print(greeting + "My name is " + name + ". I live in " + postcode + " " + city + ".");
-        // Der Unterschied zwischen print und println ist, dass println ein 'enter' hinter der Ausführung hinzufügt.
     }
 }
