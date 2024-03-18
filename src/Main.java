942ad3ddada49ece9b6b227b16e302e1a3b571aa
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2024-03-18 09:09:03 +0100

    first if else import for date

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,28 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalTime;
+
 public class Main {
     public static void main(String[] args) {
-
         String name = "Martin";
-        Number postcode = 83707;
+        int postcode = 83707;
         String city = "Bad Wiessee";
 
-        System.out.print("Hello and welcome! My name is " + name + ". I live in " + postcode + " " + city + ".");
+        LocalTime currentTime = LocalTime.now();
+        int hour = currentTime.getHour();
 
+        String greeting;
+
+        if(hour < 12 && hour > 5) {
+            greeting = "Good Morning! ";
+        } else if (hour > 12 && hour < 20) {
+            greeting = "Good Afternoon! ";
+        } else {
+            greeting = "Good Evening! ";
+        }
+
+
+        System.out.print(greeting + "My name is " + name + ". I live in " + postcode + " " + city + ".");
         // Der Unterschied zwischen print und println ist, dass println ein 'enter' hinter der Ausführung hinzufügt.
     }
 }
