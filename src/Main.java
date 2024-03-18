3efe8598a54ae48ee5862ef1ae639f9fa7ad418f
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2024-03-18 07:31:13 +0100

    begin hello World

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String name = "Martin";
+        Number postcode = 83707;
+        String city = "Bad Wiessee";
+
+        System.out.print("Hello and welcome! My name is " + name + ". I live in " + postcode + " " + city + ".");
+
+        // Der Unterschied zwischen print und println ist, dass println ein 'enter' hinter der Ausführung hinzufügt.
     }
 }
