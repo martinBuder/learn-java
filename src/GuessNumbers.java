98a666ff926fd1b6b50754b6c4a3a1430aa20bec
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2024-03-19 10:41:43 +0100

    added a counter in guess the number

--- src/GuessNumbers.java
+++ src/GuessNumbers.java
@@ -3,16 +3,18 @@ import java.util.Random;
 
 public class GuessNumbers {
 
+    static int counter = 0;
     static Random random = new Random();
     static int myNumber = random.nextInt(10) +10; //random gibt zwischen 0 und 10 aus . danach wird die +10 dazu gerechnet
 
     public static void guess() {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Rate die richtige Nummer:");
         int number = scanner.nextInt();
+        counter++;
         if(number == myNumber) {
-            System.out.println("Richtig geraten!");
+            if(counter == 1) System.out.println("Richtig geraten! Du hast " + counter + " Versuch gebraucht.");
+            else System.out.println("Richtig geraten! Du hast " + counter + " Versuche gebraucht.");
         } else {
             if(number > myNumber) System.out.println("Leider falsch. Die Zahl ist zu groß. Probiere es noch mal.");
             else System.out.println("Leider falsch. Die Zahl ist zu klein. Probiere es noch mal.");
